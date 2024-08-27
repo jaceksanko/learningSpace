e99d3e663db527eed6abda7bc7b3b7f5525b9a80
Jacek Sańko <jaceksanko@192.168.1.7>
2024-08-27 19:10:51 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import data from "./data.json";
+
+export default function App() {
+  console.log(data, "data");
+  return (
+    <div className="App">
+      <h2>Lista pracowników</h2>
+    </div>
+  );
+}
